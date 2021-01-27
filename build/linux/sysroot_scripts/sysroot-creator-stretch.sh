acea5bec4f091a56f7e88e13038a275aba9f2450
Greg Magolan <greg.magolan@robinhood.com>
2021-01-27 15:27:54 -0800

    set -x

--- build/linux/sysroot_scripts/sysroot-creator-stretch.sh
+++ build/linux/sysroot_scripts/sysroot-creator-stretch.sh
@@ -3,6 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+set -x
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DISTRO=debian
